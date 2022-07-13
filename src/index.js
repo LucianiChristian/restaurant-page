513492a59c70a64657ab3ad73f75b7ae67f82753
LucianiChristian <luciani_christian@yahoo.com>
2022-07-13 17:54:01 -0500

    First set of JS rendering complete.

--- src/index.js
+++ src/index.js
@@ -1,2 +1,16 @@
-import './style.css';
+const body = document.querySelector('body');
 
+const nav = document.createElement('nav');
+const ul = document.createElement('ul');
+const li1 = document.createElement('li');
+li1.textContent = 'Home';
+const li2 = document.createElement('li');
+li2.textContent = 'Menu';
+const li3 = document.createElement('li');
+li3.textContent = 'About';
+
+body.appendChild(nav);
+nav.appendChild(ul);
+ul.appendChild(li1);
+ul.appendChild(li2);
+ul.appendChild(li3);
