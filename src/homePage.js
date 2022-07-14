e245f040ff6209d4cbacc26352489ab5285cffe5
LucianiChristian <luciani_christian@yahoo.com>
2022-07-14 15:47:12 -0500

    Initial page load done. Utilized source mapping for debug and a webpack --watch script.

---
+++ src/homePage.js
@@ -0,0 +1,47 @@
+export default homePage;
+
+function homePage() {
+    const content = document.getElementById('content');
+
+
+    // --------- Nav Rendering --------- //
+    const nav = document.createElement('nav');
+    const ul = document.createElement('ul');
+    const li1 = document.createElement('li');
+    li1.textContent = 'Home';
+    const li2 = document.createElement('li');
+    li2.textContent = 'Menu';
+    const li3 = document.createElement('li');
+    li3.textContent = 'About';
+
+    content.appendChild(nav);
+    nav.appendChild(ul);
+    ul.appendChild(li1);
+    ul.appendChild(li2);
+    ul.appendChild(li3);
+
+    // --------- Main Rendering --------- //
+    const main = document.createElement('main');
+    const h3 = document.createElement('h3');
+    h3.textContent = 'LA PANADERIA';
+    const img = document.createElement('img');
+    img.setAttribute('src', '../src/restaurantImage.jpg');
+    const hr = document.createElement('hr');
+    const p = document.createElement('p');
+    p.textContent = 'El placer de comer con las manos. Bien tierruo!';
+    const button = document.createElement('button');
+    button.textContent = 'VER EL MENU';
+
+    content.appendChild(main);
+    main.appendChild(h3);
+    main.appendChild(img);
+    main.appendChild(hr);
+    main.appendChild(p);
+    main.appendChild(button);
+
+    // --------- Footer Rendering --------- //
+    const footer = document.createElement('footer');
+    footer.textContent = 'Developed by Christian L.';
+
+    content.appendChild(footer);
+}
--- src/index.js
+++ src/index.js
@@ -1,16 +1,3 @@
-const body = document.querySelector('body');
+import homePage from './homePage';
 
-const nav = document.createElement('nav');
-const ul = document.createElement('ul');
-const li1 = document.createElement('li');
-li1.textContent = 'Home';
-const li2 = document.createElement('li');
-li2.textContent = 'Menu';
-const li3 = document.createElement('li');
-li3.textContent = 'About';
-
-body.appendChild(nav);
-nav.appendChild(ul);
-ul.appendChild(li1);
-ul.appendChild(li2);
-ul.appendChild(li3);
+homePage();
