8e8a50c0f405a379d46560b3877c56ee809a3ba7
LucianiChristian <luciani_christian@yahoo.com>
2022-07-16 07:43:57 -0500

    Starting a basic food item  card, needs styling.

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -4,21 +4,23 @@ import restaurantImage from './restaurantImage.jpg';
 export default home;
 
 function home() {
-    const content = document.getElementById('content');
-    const main = document.querySelector('main');
-
-    // --------- Home Rendering --------- //
     const h1 = document.createElement('h1');
     h1.textContent = 'LO-FI CAFE';
+
     const img = document.createElement('img');
     img.setAttribute('src', restaurantImage);
+
     const hr = document.createElement('hr');
+
     const p = document.createElement('p');
     p.textContent = 'El placer de comer con las manos. Bien tierruo!';
+    
     const button = document.createElement('button');
     button.textContent = 'VER EL MENU';
 
-    // content.appendChild(main);
+
+    const main = document.querySelector('main');
+    
     main.appendChild(h1);
     main.appendChild(img);
     main.appendChild(hr);
--- src/components/menu/menu.js
+++ src/components/menu/menu.js
@@ -1,9 +1,42 @@
+import pizzaImage from './pizza.gif';
+
 export default menu;
 
+
 function menu() {
-    const tester = document.createElement('h1');
-    tester.textContent = 'menuPage!';
+    const card = document.createElement('div');
+    card.classList.add('card');
+
+    const cardImage = document.createElement('img');
+    cardImage.classList.add('card--image');
+    cardImage.setAttribute('src', pizzaImage);
+
+    const cardMenu = document.createElement('div');
+    cardMenu.classList.add('card--menu');
+
+    const cardMenuTitle = document.createElement('h2');
+    cardMenuTitle.classList.add('card--menu--title');
+    cardMenuTitle.textContent = 'Menu Title';
+
+    const cardMenuDescription = document.createElement('p');
+    cardMenuDescription.classList.add('card--menu--description');
+    cardMenuDescription.textContent = 'Menu Description, mmm. this that this';
+
+    const cardMenuPrice = document.createElement('p');
+    cardMenuPrice.classList.add('card--menu--price');
+    cardMenuPrice.textContent = '$23.00';
+
+    const cardMenuButton = document.createElement('button');
+    cardMenuButton.classList.add('card--menu--button');
+    cardMenuButton.textContent = 'Add to Cart';
 
     const main = document.querySelector('main');
-    main.appendChild(tester);
+
+    main.appendChild(card);
+    card.appendChild(cardImage);
+    card.appendChild(cardMenu);
+    cardMenu.appendChild(cardMenuTitle);
+    cardMenu.appendChild(cardMenuDescription);
+    cardMenu.appendChild(cardMenuPrice);
+    cardMenu.appendChild(cardMenuButton);
 }
